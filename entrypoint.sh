9ad9376d75c670add15bd7f87e9538090406185d
Plugin Drone <77864380+plugindrone@users.noreply.github.com>
2021-06-15 15:01:40 -0300

    Melhorado o mecanismo de execução de comandos

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,18 @@ else
     printenv PLUGIN_KUBE_CONFIG > ~/.kube/config
     printenv PLUGIN_CMD | sed 's/","/"\n"/g' | sed "s/','/'\n'/g" | sed 's/,/\n/g' > /tmp/cmd
     chmod +x /tmp/cmd
-    cat /tmp/cmd
+    TOTAL_COMANDOS=$(wc -l /tmp/cmd | awk '{print $1}')
+    for i in `seq 1 ${TOTAL_LINHAS}`; do
+        COMANDO_ATUAL=$(sed -n ${i}p /tmp/cmd)
+        if [ "$PLUGIN_DEBUG" == "1" ]; then
+            echo "** Executando o comando: ($COMANDO_ATUAL)"
+        fi
+        ${COMANDO_ATUAL}
+        if [ ! $? = 0 ]; then
+            if [ "$PLUGIN_DEBUG" == "1" ]; then
+                echo "Erro ao comando: ($COMANDO_ATUAL)"
+            fi
+        fi
+    done
     source /tmp/cmd
 fi
